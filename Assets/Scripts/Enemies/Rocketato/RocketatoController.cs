6059f5b67847e626d58a7d35b42bb426a08b00e9
Atomic-Potato <55362397+Atomic-Potato@users.noreply.github.com>
2022-10-07 10:50:23 +0300

    Added cooking and launch

--- Assets/Scripts/Enemies/Rocketato/RocketatoController.cs
+++ Assets/Scripts/Enemies/Rocketato/RocketatoController.cs
@@ -5,8 +5,16 @@ using Pathfinding;
 
 public class RocketatoController : MonoBehaviour
 {
+    public string targetTag = "Player";
     public float speed = 10f;
     [SerializeField] float initialRotationSpeed = 20f;
+    [Tooltip("If the value is negative distanceToCook will be used")]
+    [SerializeField] float timeToCook = 9f;
+    [Tooltip("If timeToCook is negative this will be used")]
+    [Range(0, 100f)]
+    [SerializeField] float distanceToCook = 7f;
+    [SerializeField] float cookingTime = 2.5f;
+    [SerializeField] float launchedSpeed = 100f;
     [Space]
     [SerializeField] float enemyKnockLinearDrag = 1f;
     public static float timeToDisableKnock = 1f;
@@ -23,31 +31,137 @@ public class RocketatoController : MonoBehaviour
 
     #region Private Variables
     float originalSpeed;
-    Transform playerTransform;
+    Transform targetTransform;
+
+    // State booleans
+    public bool isMoving;
+    public bool isCooking;
+    public bool isLaunched;
+    public bool isJustStopped;
+
+    //Coroutine cache
+    Coroutine cookTimeCache = null;
+
+    //References
+    float xVelocityRef = 0f;
+    float yVelocityRef = 0f;
+    float angVelocityRef = 0f;
     #endregion
 
+    #region Start and FixedUpdate
     private void Start()
     {
+        //Doing required calculations
+        speed *= 15f;
+        launchedSpeed *= 15f;
+
         //Finding required components
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        targetTransform = GameObject.FindGameObjectWithTag(targetTag).transform;
     }
 
     private void FixedUpdate()
     {
-        Move(speed, initialRotationSpeed);
-    }
+        if(!(isCooking || isLaunched))
+        {
+            if(timeToCook >= 0f)
+            {
+                if(cookTimeCache == null)
+                {
+                    Debug.Log("Started coroutine");
+                    cookTimeCache = StartCoroutine(CookInTime());
+                }
+            }
+            else
+            {
+                Debug.Log("Started cook distance check");
+                CookInDistance((Vector2)targetTransform.position);
+            }
+        }
 
+        if(!(isCooking || isLaunched))
+            Move(targetTransform.position, speed, initialRotationSpeed);
+    }
+    #endregion
 
     #region  Movement
-    private void Move(float velocity, float rotationSpeed)
+    private void Move(Vector3 targetPosition, float velocity, float rotationSpeed)
     {
-        Vector3 direction = (playerTransform.position - transform.position).normalized;
+        isMoving = true;
+
+        Vector3 direction = (targetPosition - transform.position).normalized;
         float rotationMultiplier = Vector3.Cross(direction, transform.right).z;
 
         rigidBody.angularVelocity = -rotationMultiplier * initialRotationSpeed;
         rigidBody.velocity = transform.right * velocity * Time.deltaTime;
     }
 
+    private IEnumerator Launch(float velocity)
+    {
+        isLaunched = true;
+        while(isLaunched)
+        {
+            rigidBody.velocity = transform.right * velocity * Time.deltaTime;
+            yield return null;
+        }
+    }
+    #endregion
+
+    #region  Cooking
+    private IEnumerator Cook()
+    {
+        Debug.Log("Started Cooking");
+
+        isCooking = true;
+        isMoving = false;
+
+        // Stop the rocket normal velocity and angular velocity
+        Coroutine cache = null;
+        while(!isJustStopped)
+        {
+            if(cache == null)
+                cache = StartCoroutine(Stop());
+            yield return null;
+        }
+        isJustStopped = false;
+
+        yield return new WaitForSeconds(cookingTime);
+
+        isCooking = false;
+
+        StartCoroutine(Launch(launchedSpeed));
+    }
+
+    private IEnumerator CookInTime()
+    {
+        yield return new WaitForSeconds(timeToCook);
+        StartCoroutine(Cook());
+    }
+
+    private void CookInDistance(Vector2 target)
+    {
+        if(Vector2.Distance(target, (Vector2)transform.position) <= distanceToCook)
+            StartCoroutine(Cook());
+    }
+
+    private IEnumerator Stop()
+    {
+        while(rigidBody.angularVelocity < -0.1f || rigidBody.angularVelocity > 0.1f)
+        {
+            Debug.Log("Stopping");
+            //normal velocity reduction
+            rigidBody.velocity = new Vector2(   Mathf.SmoothDamp(rigidBody.velocity.x, 0f, ref xVelocityRef, 0.25f),
+                                                Mathf.SmoothDamp(rigidBody.velocity.y, 0f, ref yVelocityRef, 0.25f));
+            //angular velocity reduction
+            rigidBody.angularVelocity = Mathf.SmoothDamp(rigidBody.angularVelocity, 0f, ref angVelocityRef, 0.25f);
+
+            yield return null;
+        }
+
+        rigidBody.velocity = new Vector2(0f,0f);
+        rigidBody.angularVelocity = 0f;
+
+        isJustStopped = true;
+    }
     #endregion
 
     private void OnTriggerEnter2D(Collider2D collider)
