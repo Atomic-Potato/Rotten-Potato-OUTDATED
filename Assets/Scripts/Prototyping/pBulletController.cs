c86d0a6c52b69179bb6221ddd4bf7a7c488c3875
Atomic Potato <medyanmehiddine@gmail.com>
2023-09-04 12:06:54 +0300

    #Prototype-1# Enemy path follow

---
+++ Assets/Scripts/Prototyping/pBulletController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class pBulletController : MonoBehaviour
+{
+    [SerializeField] float launchForce = 2f;
+    [Range(0f, 30f)]
+    [SerializeField] float lifeTime = 10f;
+
+    [Space]
+    [SerializeField] Rigidbody2D rigidbody;
+
+    void OnEnable()
+    {
+        rigidbody.AddForce(Vector2.right * launchForce, ForceMode2D.Impulse);
+
+        StartCoroutine(SelfDestructInTime());
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            DamagePlayer();
+            SelfDestroy();
+        }
+        else
+        {
+            SelfDestroy();
+        }
+    }
+
+    void DamagePlayer()
+    {
+        
+    }
+
+    void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator SelfDestructInTime()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        SelfDestroy();
+    }
+}
---
+++ Assets/Scripts/Prototyping/pEnemyShooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class pEnemyShooting : MonoBehaviour
+{
+    [SerializeField] int timeBetweenShots;
+    [SerializeField] bool shootOnStart;
+    [SerializeField] GameObject bulletObject;
+
+    IEnumerator Shoot()
+    {
+        if (shootOnStart)
+        {
+
+        }
+    }
+
+    void LaunchBullet()
+    {
+        Instantiate(bulletObject, transform.position, quaternion.identity);
+    }
+}
