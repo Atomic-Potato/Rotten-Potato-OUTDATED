20c627904e7d7c70fa9ff41ccdc3e026616e135b
Atomic Potato <medyanmehiddine@gmail.com>
2023-09-07 17:33:27 +0300

    #Prototype-1# Aim Indicator

---
+++ Assets/Scripts/Prototyping/pAimIndicator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class pAimIndicator : MonoBehaviour
+{
+    [Range(0f, 20f)]
+    [SerializeField] float distanceFromPlayer = 5f;
+    [SerializeField] GameObject indicator;
+
+    [Space]
+    [Header("Gizmos")]
+    [SerializeField] bool gizmos;
+    [SerializeField] bool gizmosDisplayMouseDirection;
+    [SerializeField] bool gizmosDisplayIndicatorDirection;
+
+    void OnDrawGizmos()
+    {
+        if (!gizmos)
+        {
+            return;
+        }
+
+
+        if (gizmosDisplayMouseDirection)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float distanceFromPlayer = Vector3.Distance(transform.position, mousePosition);
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(transform.position, GetDirectionToMouse() * distanceFromPlayer);
+        }
+
+        if (gizmosDisplayIndicatorDirection)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, GetDirectionToMouse() * distanceFromPlayer);
+        }
+    }
+
+    void Update()
+    {
+        indicator.transform.position = (Vector2)transform.position + GetDirectionToMouse() * distanceFromPlayer;
+    }
+
+    Vector2 GetDirectionToMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - transform.position;
+        direction.Normalize();
+        return direction;
+    }
+}
