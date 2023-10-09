3adececc1fea83df9eb114ed12c4ffd758d6c74c
Atomic Potato <medyanmehiddine@gmail.com>
2023-10-09 16:52:32 +0300

    #Prototype-1# Respawn delay

--- Assets/Scripts/Game Systems/SpawnManager.cs
+++ Assets/Scripts/Game Systems/SpawnManager.cs
@@ -1,12 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [Range(0f, 100f)]
+    [SerializeField] float timeToSpawn = 1f; 
     [SerializeField] GameObject playerPrefab;
 
     public static SpawnManager Instance;
     [HideInInspector] public Vector2? LevelSpawnPoint;
     [HideInInspector] public Vector2? Checkpoint;
+
+    GameObject _spawnedPlayerObject;
+    Coroutine _spawnCache;
 
     void Awake() 
     {
@@ -28,7 +34,17 @@ public class SpawnManager : MonoBehaviour
         if (LevelSpawnPoint == null)
             throw new System.Exception("Failed to spawn player: No spawn point set");
 
-        Instantiate(playerPrefab, (Vector2)LevelSpawnPoint, Quaternion.identity);
+        if (_spawnCache == null)
+            _spawnCache = StartCoroutine(ExecuteSpawn());
+
+        IEnumerator ExecuteSpawn()
+        {
+            _spawnedPlayerObject = Instantiate(playerPrefab, (Vector2)LevelSpawnPoint, Quaternion.identity);
+            _spawnedPlayerObject.SetActive(false);
+            yield return new WaitForSecondsRealtime(timeToSpawn);
+            _spawnedPlayerObject.SetActive(true);
+            _spawnCache = null;
+        }
     }
 
     public void SpawnPlayerAtCheckpoint()
@@ -39,6 +55,16 @@ public class SpawnManager : MonoBehaviour
         if (Checkpoint == null)
             throw new System.Exception("Failed to spawn player: No checkpoint set");
 
-        Instantiate(playerPrefab, (Vector2)Checkpoint, Quaternion.identity);
+        if (_spawnCache == null)
+            _spawnCache = StartCoroutine(ExecuteSpawn());
+
+        IEnumerator ExecuteSpawn()
+        {
+            _spawnedPlayerObject = Instantiate(playerPrefab, (Vector2)Checkpoint, Quaternion.identity);
+            _spawnedPlayerObject.SetActive(false);
+            yield return new WaitForSecondsRealtime(timeToSpawn);
+            _spawnedPlayerObject.SetActive(true);
+            _spawnCache = null;
+        }
     }
 }
